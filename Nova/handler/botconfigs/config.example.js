65c71a821789e234cad4005d6c8b44a114097ac7
discord tutorial <92290826+duckytutorials@users.noreply.github.com>
2022-07-23 20:15:26 +0630

    leaks

--- Nova/handler/botconfigs/config.example.js
+++ Nova/handler/botconfigs/config.example.js
@@ -13,5 +13,5 @@ module.exports = {
         errorMessage: ["", "$log[[ERROR] :: $username had a error in $servername | ERROR ID: $randomString[$random[4;10]]]{newEmbed:{title:Ah oh!}{description:There was a error | ERROR ID: $randomString[$random[4;10]]}{color:fcbfcb}}"]
     },
     git: {
-        PAT: "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"
+        PAT: process.env.pat
     }}
